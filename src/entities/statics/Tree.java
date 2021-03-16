6f0200eed67f5e723e738c56a8e8fdda24a58e75
Hanh Ngô <38860847+hanh-nd@users.noreply.github.com>
2021-03-16 23:39:08 +0700

    improve tree image

--- src/entities/statics/Tree.java
+++ src/entities/statics/Tree.java
@@ -3,13 +3,20 @@ package entities.statics;
 import game.Handler;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import settings.Settings;
 
 public class Tree extends StaticEntity{
 
+    Image tree;
+
     public Tree(Handler handler, Image image, float x, float y){
         super(handler, image, x, y, Settings.TILE_WIDTH, (int) (Settings.TILE_HEIGHT*2*0.7));
+
+        imageView = new ImageView(image);
+        imageView.setFitHeight(height);
+        imageView.setPreserveRatio(true);
 
         bounds.setX(36);
         bounds.setY(65);
@@ -24,11 +31,9 @@ public class Tree extends StaticEntity{
 
     @Override
     public void render(GraphicsContext g) {
-//        imageView.setFitHeight(height);
-//        imageView.setPreserveRatio(true);
-//        imageView.relocate((int)(x - handler.getGameCamera().getxOffset()), (int) (y - handler.getGameCamera().getyOffset()));
 
-        g.drawImage(image, (int)(x - handler.getGameCamera().getxOffset()),
-                (int) (y - handler.getGameCamera().getyOffset()), width, height);
+        tree = imageView.snapshot(params, null);
+        g.drawImage(tree, (int)(x - handler.getGameCamera().getxOffset()),
+                (int) (y - handler.getGameCamera().getyOffset()));
     }
 }
